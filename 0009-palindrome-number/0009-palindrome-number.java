758d93eeba106c758abd5237b42e887df090c2ae
hg0710 <122251142+hg0710@users.noreply.github.com>
2023-08-15 17:13:32 +0900

    Time: 7 ms (74.10%), Space: 42.7 MB (83.65%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.java
@@ -0,0 +1,11 @@
+class Solution {
+    public boolean isPalindrome(int x) {
+        
+        if(x < 0 ) return false;
+
+        String s =  String.valueOf(x);
+    
+        return s.equals(new StringBuilder(s).reverse().toString());
+
+    }
+}
